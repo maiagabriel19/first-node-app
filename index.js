4a84b8fcd369d583c635015347fe1d4d83ae5289
maiagabriel19 <gabrielmaia190703@gmail.com>
2025-02-12 16:52:11 -0300

    add read file

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ let writeStream = fs.writeFileSync("./teste.txt", "ola mundo", erro => {
         console.log(erro);
     }
 } ); 
+
+fs.readFile("./teste.txt", "utf-8", (erro, dados) => {
+    console.log(dados);
+});
