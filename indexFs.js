bbd16def85116dd22b379c40a9a06632ba9e1281
maiagabriel19 <gabrielmaia190703@gmail.com>
2025-02-25 17:07:24 -0300

    refatora conteudo do arquivo

--- indexFs.js
+++ indexFs.js
@@ -1,12 +1,15 @@
 import { error } from "console";
 import fs from "fs";
 
-let writeFileSync = fs.writeFileSync("./conteudo.txt","Escrevendo texto com a biblioteca Fs\n", erro => {
+let conteudoFs = "Escrevendo texto com a biblioteca Fs\n";
+let nodeFs = "Aprendendo NodeJs";
+
+let writeFileSync = fs.writeFileSync("./conteudo.txt", conteudoFs, erro => {
     if (erro){
         console.log("erro")
     }
 });
-let write = fs.appendFile("./conteudo.txt","Aprendendo NodeJs", erro =>{
+let write = fs.appendFile("./conteudo.txt", nodeFs, erro =>{
     if (erro){
         console.log("erro");
     }
