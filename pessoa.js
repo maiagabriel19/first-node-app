58437877b7c0227fd9dd860dc7ab188e7d63a06c
maiagabriel19 <gabrielmaia190703@gmail.com>
2025-02-27 13:53:24 -0300

    adiciona classe pessoa

---
+++ pessoa.js
@@ -0,0 +1,24 @@
+class Pessoa {
+    altura;
+    nome;
+    peso;
+    corDaPele;
+    corDosOlhos;
+    tamanhoDoPe;
+
+    falar(){
+        console.log("Estou falando");
+    }
+
+    andar(){
+        console.log("Voçê está andando");
+    }
+
+    correr(){
+        console.log("Voçê está correndo");
+    }
+
+    pular(){
+        console.log("pule");
+    }
+};
